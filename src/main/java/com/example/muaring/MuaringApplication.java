ca950e6089ce3a212a332bc5b271f97beb9d15e8
romdy <kimjh0937@gmail.com>
2025-10-02 22:56:21 +0900

    feat: 초기 프로젝트 세팅

---
+++ src/main/java/com/example/muaring/MuaringApplication.java
@@ -0,0 +1,13 @@
+package com.example.muaring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuaringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuaringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/muaring/MuaringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muaring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuaringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
