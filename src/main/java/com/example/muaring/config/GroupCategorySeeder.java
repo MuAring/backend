7302589a08a2e2e9003dd303983edfc9d0120427
ownue <enu_i@naver.com>
2025-10-20 16:57:24 +0900

    #9 refactor: 그룹 카테고리 seed 로직 리팩토링
    - findAll()을 통해 DB 접근 횟수 감축

--- src/main/java/com/example/muaring/config/GroupCategorySeeder.java
+++ src/main/java/com/example/muaring/config/GroupCategorySeeder.java
@@ -8,6 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Component
 @RequiredArgsConstructor
 public class GroupCategorySeeder {
@@ -17,10 +22,23 @@ public class GroupCategorySeeder {
     @PostConstruct
     @Transactional
     public void init() {
-        for (GroupCategoryType type : GroupCategoryType.values()) {
-            if (!repo.existsByName(type.getName())) {
-                repo.save(new GroupCategory(type.getName()));
-            }
+        // DB에서 한 번에 모든 카테고리 조회
+        List<GroupCategory> existingCategories = repo.findAll();
+        
+        // Set으로 변환해 중복 (있다면) 제거
+        Set<String> existingCategoryNames = existingCategories.stream()
+                .map(GroupCategory::getName)
+                .collect(Collectors.toSet());
+
+        // DB에 없는 값 필터링해서 새로운 엔티티 리스트 생성
+        List<GroupCategory> newCategories = Arrays.stream(GroupCategoryType.values())
+                .filter(type -> !existingCategoryNames.contains(type.getName()))
+                .map(type -> new GroupCategory(type.getName()))
+                .collect(Collectors.toList());
+
+        // 전부 추가
+        if (!newCategories.isEmpty()) {
+            repo.saveAll(newCategories);
         }
     }
 }
