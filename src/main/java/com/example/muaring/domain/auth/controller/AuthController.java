58f605a201fb57dabe03869cd779ef0ce261f48e
hyomee2 <s5763305@naver.com>
2025-10-25 01:58:26 +0900

    chore: 응답 구조 통일

--- src/main/java/com/example/muaring/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/muaring/domain/auth/controller/AuthController.java
@@ -19,12 +19,13 @@ public class AuthController {
 
     private final AuthService kakaoAuthService;
 
-    // ⚪ 카카오 서버로부터 요청되는 엔드포인트 (리다이렉트)
+    // ⚪ 카카오 서버로부터 리다이렉트 되는 엔드포인트 (테스트용)
     @GetMapping("/login/kakao")
     @Operation(summary = "카카오 인가 코드 수신", description = "카카오 서버로부터 요청되는 인가 코드 수신 로직입니다.")
-    public ResponseEntity<String> kakaoLoginRedirect(String code) {
-        log.info("카카오 인가 코드 수신: {}", code);
-        return ResponseEntity.ok("인가 코드: " + code);
+    public ResponseEntity<ApiResponse<String>> kakaoLoginRedirect(String code) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.ok("인가 코드가 생성되었습니다."));
     }
 
     @Operation(summary = "카카오 소셜 로그인", description = "카카오 소셜 로그인 로직입니다.")
