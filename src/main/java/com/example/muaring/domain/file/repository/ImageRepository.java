dd6cd297c051b02f9003950a2c8e90a492f10c6f
hyomee2 <s5763305@naver.com>
2025-10-30 15:57:07 +0900

    chore: @Repository 어노테이션 추가

--- src/main/java/com/example/muaring/domain/file/repository/ImageRepository.java
+++ src/main/java/com/example/muaring/domain/file/repository/ImageRepository.java
@@ -2,6 +2,8 @@ package com.example.muaring.domain.file.repository;
 
 import com.example.muaring.domain.file.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image,Long> {
 }
