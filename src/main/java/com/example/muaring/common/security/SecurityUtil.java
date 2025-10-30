8366ada221fe006a9ad9df0c1e3a856db015dcde
hyomee2 <s5763305@naver.com>
2025-10-30 15:54:32 +0900

    refactor: securityUtil 외부 인스턴스화 방지 추가

--- src/main/java/com/example/muaring/common/security/SecurityUtil.java
+++ src/main/java/com/example/muaring/common/security/SecurityUtil.java
@@ -3,18 +3,19 @@ package com.example.muaring.common.security;
 import com.example.muaring.domain.auth.exception.AuthException;
 import com.example.muaring.domain.auth.exception.AuthErrorCode;
 import com.example.muaring.domain.auth.security.MemberPrincipal;
-import lombok.experimental.UtilityClass;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 // ✨현재 로그인한 사용자 정보를 꺼낼 수 있도록 해주는 클래스
-@Slf4j
-@UtilityClass
-public class SecurityUtil {
+public final class SecurityUtil {
+
+    // 외부 인스턴스화 방지
+    private SecurityUtil() {
+        throw new UnsupportedOperationException("SecurityUtil은 유틸리티 클래스이므로 인스턴스화할 수 없습니다.");
+    }
 
     // ⚪ SecurityContext에서 Authentication 객체 가져오기
-    private Authentication getAuthentication() {
+    private static Authentication getAuthentication() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || authentication.getPrincipal() == null) {
@@ -24,7 +25,7 @@ public class SecurityUtil {
     }
 
     // ⚪ 로그인한 사용자의 memberId 반환
-    public Long getMemberId() {
+    public static Long getMemberId() {
         Authentication auth = getAuthentication();
 
         Object principal = auth.getPrincipal();
@@ -38,7 +39,7 @@ public class SecurityUtil {
     }
 
     // ⚪ 로그인한 사용자의 email 반환
-    public String getMemberEmail() {
+    public static String getMemberEmail() {
         Authentication auth = getAuthentication();
 
         Object principal = auth.getPrincipal();
@@ -50,7 +51,7 @@ public class SecurityUtil {
     }
 
     // ⚪ 로그인한 사용자의 access token 반환
-    public String getMemberAccessToken() {
+    public static String getMemberAccessToken() {
         Authentication auth = getAuthentication();
         if (auth.getCredentials() instanceof String token) {
             return token;
