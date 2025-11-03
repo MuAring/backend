5f48bc2cd96b0a6cf84bf90903592b650471caee
hyomee2 <s5763305@naver.com>
2025-11-04 02:16:47 +0900

    chore: 중복 code 수정

--- src/main/java/com/example/muaring/domain/member/response/MemberErrorCode.java
+++ src/main/java/com/example/muaring/domain/member/response/MemberErrorCode.java
@@ -13,11 +13,11 @@ public enum MemberErrorCode implements ErrorCode {
     ALREADY_HAS_PROFILE(2001, HttpStatus.BAD_REQUEST, "이미 프로필 정보가 존재하는 회원입니다."),
 
     // 404 에러
-    MEMBER_NOT_FOUND(2001, HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
-    METRICS_NOT_FOUND(2002, HttpStatus.NOT_FOUND, "핵심 필드를 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(2002, HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
+    METRICS_NOT_FOUND(2003, HttpStatus.NOT_FOUND, "핵심 필드를 찾을 수 없습니다."),
 
     // 409 에러
-    METRICS_CONFLICT(2003, HttpStatus.CONFLICT, "핵심 필드가 존재할 수 없습니다.");
+    METRICS_CONFLICT(2004, HttpStatus.CONFLICT, "핵심 필드가 존재할 수 없습니다.");
 
     private final int code;
     private final HttpStatus status;
