2ee013190f778ebc342276a1257718f0c5a69f0b
yyen <brenda1217@naver.com>
2025-11-04 02:38:05 +0900

    #21 [feat]: 응답 방식 수정

--- src/main/java/com/example/muaring/domain/music/exception/MusicException.java
+++ src/main/java/com/example/muaring/domain/music/exception/MusicException.java
@@ -1,14 +1,13 @@
 package com.example.muaring.domain.music.exception;
 
+import com.example.muaring.common.exception.GeneralException;
 import com.example.muaring.common.response.ErrorCode;
 import lombok.*;
 
 @Getter
-public class MusicException extends RuntimeException {
-    private final ErrorCode errorCode;
+public class MusicException extends GeneralException {
 
     public MusicException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
+        super(errorCode);
     }
 }
