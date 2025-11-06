392ccbb685eb23152ff73941172d10ee375db998
hyomee2 <s5763305@naver.com>
2025-11-06 22:25:19 +0900

    refactor: ok 응답구조 통일

--- src/main/java/com/example/muaring/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/muaring/domain/auth/controller/AuthController.java
@@ -7,7 +7,6 @@ import com.example.muaring.domain.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,8 +23,7 @@ public class AuthController {
     @Operation(summary = "카카오 인가 코드 수신", description = "카카오 서버로부터 요청되는 인가 코드 수신 로직입니다.")
     public ResponseEntity<ApiResponse<String>> kakaoLoginRedirect(String code) {
         return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ApiResponse.ok("인가 코드가 생성되었습니다."));
+                .ok(ApiResponse.ok("인가 코드가 생성되었습니다."));
     }
 
     @Operation(summary = "카카오 소셜 로그인", description = "카카오 소셜 로그인 로직입니다.")
@@ -34,7 +32,6 @@ public class AuthController {
         log.info("카카오 로그인 요청 수신");
         LoginResponseDTO response = kakaoAuthService.login(socialLoginRequestDTO.code(), socialLoginRequestDTO.authProvider());
         return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ApiResponse.ok(response, socialLoginRequestDTO.authProvider() + "로그인에 성공했습니다."));
+                .ok(ApiResponse.ok(response, socialLoginRequestDTO.authProvider() + "로그인에 성공했습니다."));
     }
 }
