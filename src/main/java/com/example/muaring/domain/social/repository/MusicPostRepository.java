9e59bcba157f4710738b97eb830ec013cc42ff05
ownue <enu_i@naver.com>
2025-11-17 17:42:47 +0900

    feat: 그룹 프로필 조회, 그룹 피드 조회

--- src/main/java/com/example/muaring/domain/social/repository/MusicPostRepository.java
+++ src/main/java/com/example/muaring/domain/social/repository/MusicPostRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+
 public interface MusicPostRepository extends JpaRepository<MusicPost, Long> {
 
     @Query("""
@@ -17,11 +19,24 @@ public interface MusicPostRepository extends JpaRepository<MusicPost, Long> {
       AND MONTH(mp.createdAt) = :month
     ORDER BY mp.createdAt DESC
     """)
-
     Page<MusicPost> findByMemberAndYearMonth(
             @Param("memberId") Long memberId,
             @Param("year") Integer year,
             @Param("month") Integer month,
+            Pageable pageable
+    );
+
+    @Query("select count(mp) from MusicPost mp where mp.group.id = :groupId and mp.isDeleted = false")
+    int countActiveByGroupId(@Param("groupId") Long groupId);
+
+    // 전체 조회 (그룹 기준)
+    Page<MusicPost> findByGroup_Id(Long groupId, Pageable pageable);
+
+    // 특정 월 범위 조회
+    Page<MusicPost> findByGroup_IdAndCreatedAtBetween(
+            Long groupId,
+            LocalDateTime start,
+            LocalDateTime end,
             Pageable pageable
     );
 }
